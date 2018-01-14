26bb5e65ff3c004cd1d9ca9dc8c68f9e80b4097f
Chen Chen <chenchenstudent@gmail.com>
2018-01-14 09:16:50 +0800

    better solution for question 216

--- 216.cpp
+++ 216.cpp
@@ -1,39 +1,21 @@
 #inclde "mod.h"
 class Solution {
-    set<set<int>>result;
-    void helper(set<int> mySet, const int& k, const int &n, int cur_val){
-        if(mySet.size()==k){
-            int temp=n-cur_val;
-            if(temp<=9&&temp>=1&&mySet.find(temp)==mySet.end()){
-                mySet.insert(temp);
-                if(result.find(mySet)==result.end()){
-                    result.insert(mySet);
-                }
-            }
-        }
-        else{
-            for(int num=1; num<=9; ++num){
-                if(mySet.find(num)==mySet.end()){
-                    mySet.insert(num);
-                    helper(mySet, k, n, cur_val+num);
-                    mySet.erase(num);
-                }
-            }
+    vector<vector<int>>result;
+    void helper(vector<int> temp, const int& k, int cur_val, int level){
+        int len=temp.size();
+        if(cur_val<0||len>k)return;
+        if(cur_val==0&&len==k)result.push_back(temp);
+        for(int num=level; num<=9; ++num){
+            temp.push_back(num);
+            helper(temp, k, cur_val-num, num+1);
+            temp.pop_back();
         }
     }
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         result.clear();
-        set<int>temp;
-        helper(temp, k-1, n, 0);
-        vector<vector<int>> ret(result.size(), vector<int>(k, 0));
-        int ret_index=0;
-        for(auto sit=result.begin(); sit!=result.end(); (++sit, ++ret_index)){
-            int index=0;
-            for(auto ssit=sit->begin(); ssit!=sit->end(); (++ssit, ++index)){
-                ret[ret_index][index]=*ssit;
-            }
-        }
-        return ret;    
+        vector<int>temp;
+        helper(temp, k, n, 1);
+        return result;    
     }
 };
