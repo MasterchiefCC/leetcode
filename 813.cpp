e218a2db2ed050fcee2a61924ae066384dea1d5b
MasterchiefCC <chenchenstudent@gmail.com>
2018-05-05 14:30:17 -0700

    update

---
+++ 813.cpp
@@ -0,0 +1,12 @@
+#include "mod.h"
+//top->down
+class Solution {
+    double helper(const vector<int> &sum, vector<int> &A, vector<vector<double>> &dp, int k, int i){
+        if(k==1)return sum[i]/i;
+        
+    }
+public:
+    double largestSumOfAverages(vector<int>& A, int K) {
+        
+    }
+};
