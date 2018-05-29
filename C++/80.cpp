26d6d68e97f822bedfe6d12bd21ce4bee7cfaec7
MasterchiefCC <chenchenstudent@gmail.com>
2018-05-29 14:49:41 -0700

    2018.5.19

--- C++/80.cpp
+++ C++/80.cpp
@@ -20,3 +20,24 @@ public:
         return nums.size();
     }
 };
+
+class Solution2 {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int cur=INT_MIN, cur_cnt=0, j=0;
+        for(int i=0; i<nums.size(); ++i){
+            if(cur!=nums[i]){
+                cur=nums[i];
+                cur_cnt=1;
+                nums[i-j]=nums[i];
+            }
+            else if(cur==nums[i]&&cur_cnt==1){
+                ++cur_cnt;
+                nums[i-j]=nums[i];
+            }
+            else ++j;
+        }
+        nums.resize(nums.size()-j);
+        return nums.size();
+    }
+};
