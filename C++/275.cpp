8bce6471751bc6d3b86fb540d05b4ff8897ea270
MasterchiefCC <chenchenstudent@gmail.com>
2018-06-07 14:46:00 -0700

    275 update

--- C++/275.cpp
+++ C++/275.cpp
@@ -2,6 +2,13 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
+    //     int len=citations.size();
+    //     for(int i=len; i>0; --i){
+    //         if(citations[len-i]>=i)return i;
+    //     }
+    //     return 0;
+    // }
+
         int left=0, right=citations.size()-1, len=citations.size();
         while(left<=right){
             int mid=left+(right-left)/2;
