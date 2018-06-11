6c1b5b61364ad6963ff2d85ea6a790f1c0b36185
MasterchiefCC <chenchenstudent@gmail.com>
2018-06-10 20:48:04 -0700

    2018.6.10

--- C++/106.cpp
+++ C++/106.cpp
@@ -13,26 +13,24 @@ public:
 	TreeNode * buildTree(vector<int>& inorder, vector<int>& postorder) {
         if(inorder.empty())return NULL;
         
-		vector<bool>used(inorder.size(), false);
 		int p_index = postorder.size() - 1;
         
-		return constr_tree(inorder, postorder, used, p_index, 0, inorder.size());
+		return constr_tree(inorder, postorder, p_index, 0, inorder.size());
 	}
 private:
-	TreeNode * constr_tree(const vector<int>& inorder, const vector<int>& postorder, vector<bool>& used, int &p_index, int start, int end) {
+	TreeNode * constr_tree(const vector<int>& inorder, const vector<int>& postorder, int &p_index, int start, int end) {
+        if(start==end)return NULL;
 		if (p_index<0)return NULL;
 
 		const int& cur = postorder[p_index];
 		int mid = start;
 		while (mid<inorder.size() && inorder[mid] != cur)++mid;
-		if (mid >= used.size() || used[mid] == true)return NULL;
 
-		used[mid] = true;
 		TreeNode*root = new TreeNode(cur);
 		--p_index;
 
-		root->right = constr_tree(inorder, postorder, used, p_index, mid + 1, end);
-		root->left = constr_tree(inorder, postorder, used, p_index, start, mid);
+		root->right = constr_tree(inorder, postorder, p_index, mid + 1, end);
+		root->left = constr_tree(inorder, postorder, p_index, start, mid);
 
 		return root;
 	}
